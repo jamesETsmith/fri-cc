cbb7e33b0439812e454cb8c19236df80499754bb
James E T Smith <james.smith9113@gmail.com>
2021-02-15 23:41:42 -0500

    t1 now working

--- src/rccsd.cpp
+++ src/rccsd.cpp
@@ -10,13 +10,17 @@ void update_amps(
     Eigen::Ref<Eigen::VectorXd> ovvo_vec, Eigen::Ref<Eigen::VectorXd> ovov_vec,
     Eigen::Ref<Eigen::VectorXd> ovvv_vec, Eigen::Ref<Eigen::VectorXd> vvvv_vec,
     Eigen::Ref<Eigen::VectorXd> mo_energies) {
-        
+
   // Helpers
   Eigen::array<Eigen::IndexPair<int>, 1> contraction_dims_1d;
   Eigen::array<Eigen::IndexPair<int>, 2> contraction_dims_2d;
   Eigen::array<Eigen::IndexPair<int>, 3> contraction_dims_3d;
   Eigen::array<int, 2> shuffle_idx_2d;
   Eigen::array<int, 4> shuffle_idx_4d;
+    RowTensor2d tmp_2d;
+  RowTensor4d tmp_4d;
+  shuffle_idx_2d = {1, 0};
+
 
   //
   // Unpack input args and map to tensors
@@ -29,8 +33,8 @@ void update_amps(
   TMap4d t2_new(t2_vec_new.data(), nocc, nocc, nvirt, nvirt);
 
   TMap2d fock_oo(fock_oo_mat.data(), nocc, nocc);
-  TMap2d fock_ov(fock_oo_mat.data(), nocc, nvirt);
-  TMap2d fock_vv(fock_oo_mat.data(), nvirt, nvirt);
+  TMap2d fock_ov(fock_ov_mat.data(), nocc, nvirt);
+  TMap2d fock_vv(fock_vv_mat.data(), nvirt, nvirt);
 
   TMap4d oooo(oooo_vec.data(), nocc, nocc, nocc, nocc);
   TMap4d ovoo(ovoo_vec.data(), nocc, nvirt, nocc, nocc);
@@ -44,8 +48,9 @@ void update_amps(
 
   // Move energy terms to the other side
   RowTensor2d Foo(nocc, nocc);
-  RowTensor2d Fov(nocc, nocc);
-  RowTensor2d Fvv(nocc, nocc);
+  RowTensor2d Fov(nocc, nvirt);
+  RowTensor2d Fvv(nvirt, nvirt);
+  Foo.setZero(), Fov.setZero(), Fvv.setZero();
 
   Eigen::Map<RowMatrixXd> Foo_mat(Foo.data(), nocc, nocc);
   Eigen::Map<RowMatrixXd> Fov_mat(Fov.data(), nocc, nvirt);
@@ -60,13 +65,90 @@ void update_amps(
   Foo_mat.diagonal() = Foo_mat.diagonal() - mo_e_o;
   Fvv_mat.diagonal() = Fvv_mat.diagonal() - mo_e_v;
 
-  // T1 Equation update
+  //
+  // T1 update
+  //
+
+
   // -2*np.einsum('kc,ka,ic->ia', fov, t1, t1)
   contraction_dims_1d = {Eigen::IndexPair<int>(0, 0)};
-  RowTensor2d tmp_2d = 2 * fock_ov.contract(t1, contraction_dims_1d);
+  tmp_2d = fock_ov.contract(t1, contraction_dims_1d);
   contraction_dims_1d = {Eigen::IndexPair<int>(0, 1)};
-  shuffle_idx_2d = {1, 0};
-  t1_new -= tmp_2d.contract(t1, contraction_dims_1d).shuffle(shuffle_idx_2d);
+  t1_new -= 2 * tmp_2d.contract(t1, contraction_dims_1d).shuffle(shuffle_idx_2d);
+
+  // np.einsum('ac,ic->ia', Fvv, t1)
+  contraction_dims_1d = {Eigen::IndexPair<int>(1,1)};
+  t1_new += Fvv.contract(t1, contraction_dims_1d).shuffle(shuffle_idx_2d);
+
+  // -np.einsum('ki,ka->ia', Foo, t1)
+  contraction_dims_1d = {Eigen::IndexPair<int>(0,0)};
+  t1_new -= Foo.contract(t1, contraction_dims_1d);
+
+// t1new += 2*np.einsum('kc,kica->ia', Fov, t2)
+  contraction_dims_2d = {Eigen::IndexPair<int>(0,0), Eigen::IndexPair<int>(1,2)};
+  t1_new +=  2 * Fov.contract(t2, contraction_dims_2d);
+
+// t1new +=  -np.einsum('kc,ikca->ia', Fov, t2)
+  contraction_dims_2d = {Eigen::IndexPair<int>(0,1), Eigen::IndexPair<int>(1,2)};
+    t1_new -= Fov.contract(t2, contraction_dims_2d);
+  
+// t1new +=   np.einsum('kc,ic,ka->ia', Fov, t1, t1)
+    contraction_dims_1d = {Eigen::IndexPair<int>(1,1)};
+    tmp_2d = Fov.contract(t1, contraction_dims_1d);
+    contraction_dims_1d = {Eigen::IndexPair<int>(0,0)};
+    t1_new += tmp_2d.contract(t1, contraction_dims_1d);
+ 
+    // t1new += fov.conj()
+    t1_new += fock_ov;
+
+    // t1new += 2*np.einsum('kcai,kc->ia', eris.ovvo, t1)
+    contraction_dims_2d = {Eigen::IndexPair<int>(0,0), Eigen::IndexPair<int>(1,1)};
+    t1_new += 2 * ovvo.contract(t1, contraction_dims_2d).shuffle(shuffle_idx_2d);
+
+    // t1new +=  -np.einsum('kiac,kc->ia', eris.oovv, t1)
+    contraction_dims_2d = {Eigen::IndexPair<int>(0,0), Eigen::IndexPair<int>(3,1)};
+    t1_new -= oovv.contract(t1, contraction_dims_2d);
+
+    // t1new += 2*lib.einsum('kdac,ikcd->ia', eris_ovvv, t2)
+    contraction_dims_3d = {Eigen::IndexPair<int>(0,1), Eigen::IndexPair<int>(1,3), Eigen::IndexPair<int>(3,2)};
+    t1_new += 2 * ovvv.contract(t2, contraction_dims_3d).shuffle(shuffle_idx_2d);
+
+    // t1new +=  -lib.einsum('kcad,ikcd->ia', eris_ovvv, t2)
+    contraction_dims_3d = {Eigen::IndexPair<int>(0,1), Eigen::IndexPair<int>(1,2), Eigen::IndexPair<int>(3,3)};
+    t1_new -= ovvv.contract(t2, contraction_dims_3d).shuffle(shuffle_idx_2d);
+
+    // t1new += 2*lib.einsum('kdac,kd,ic->ia', eris_ovvv, t1, t1)
+    contraction_dims_2d = {Eigen::IndexPair<int>(0,0), Eigen::IndexPair<int>(1,1)};
+    tmp_2d = 2 * ovvv.contract(t1, contraction_dims_2d);
+    contraction_dims_1d = {Eigen::IndexPair<int>(1,1)};
+    t1_new += tmp_2d.contract(t1, contraction_dims_1d).shuffle(shuffle_idx_2d);
+
+    // t1new +=  -lib.einsum('kcad,kd,ic->ia', eris_ovvv, t1, t1)
+    contraction_dims_2d = {Eigen::IndexPair<int>(0,0), Eigen::IndexPair<int>(3,1)};
+    tmp_2d = ovvv.contract(t1, contraction_dims_2d);
+    contraction_dims_1d = {Eigen::IndexPair<int>(0,1)};
+    t1_new -= tmp_2d.contract(t1, contraction_dims_1d).shuffle(shuffle_idx_2d);
+
+    // t1new += -2 * lib.einsum("lcki,klac->ia", eris_ovoo, t2)
+    contraction_dims_3d = {Eigen::IndexPair<int>(0,1), Eigen::IndexPair<int>(1,3), Eigen::IndexPair<int>(2,0)};
+    t1_new -= 2 * ovoo.contract(t2, contraction_dims_3d);
+
+    // t1new += lib.einsum("kcli,klac->ia", eris_ovoo, t2)
+    contraction_dims_3d = {Eigen::IndexPair<int>(0,0), Eigen::IndexPair<int>(1,3), Eigen::IndexPair<int>(2,1)};
+    t1_new += ovoo.contract(t2, contraction_dims_3d);
+
+    // t1new += -2 * lib.einsum("lcki,lc,ka->ia", eris_ovoo, t1, t1)
+    contraction_dims_2d = {Eigen::IndexPair<int>(0,0), Eigen::IndexPair<int>(1,1)};
+    tmp_2d = 2 * ovoo.contract(t1, contraction_dims_2d);
+    contraction_dims_1d = {Eigen::IndexPair<int>(0,0)};
+    t1_new -= tmp_2d.contract(t1, contraction_dims_1d);
+
+    // t1new += lib.einsum("kcli,lc,ka->ia", eris_ovoo, t1, t1)
+    contraction_dims_2d = {Eigen::IndexPair<int>(2,0), Eigen::IndexPair<int>(1,1)};
+    tmp_2d = ovoo.contract(t1, contraction_dims_2d);
+    contraction_dims_1d = {Eigen::IndexPair<int>(0,0)};
+    t1_new += tmp_2d.contract(t1, contraction_dims_1d);
+
 }
 
 } // namespace RCCSD
--- tests/test_rupdate_amps.py
+++ tests/test_rupdate_amps.py
@@ -3,10 +3,16 @@ from pyscf import gto, scf, cc, lib
 from pyscf.cc import rintermediates as imd
 from pyscf.cc import ccsd
 
-from py_ccsd import update_amps
+from py_ccsd import update_amps as my_update_amps
 
 
 def update_amps(cc, t1, t2, eris):
+
+    intermediates = {
+        "t1_new": None,
+        "t2_new": None,
+    }  # data that we'll return as we check things line by line
+
     # Ref: Hirata et al., J. Chem. Phys. 120, 2581 (2004) Eqs.(35)-(36)
     assert isinstance(eris, ccsd._ChemistsERIs)
     nocc, nvir = t1.shape
@@ -24,7 +30,9 @@ def update_amps(cc, t1, t2, eris):
 
     # Move energy terms to the other side
     Foo[np.diag_indices(nocc)] -= mo_e_o
+    print(Fvv[np.diag_indices(nvir)])
     Fvv[np.diag_indices(nvir)] -= mo_e_v
+    print(Fvv[np.diag_indices(nvir)])
 
     # T1 equation
     t1new = -2 * np.einsum("kc,ka,ic->ia", fov, t1, t1)
@@ -46,67 +54,69 @@ def update_amps(cc, t1, t2, eris):
     t1new += lib.einsum("kcli,klac->ia", eris_ovoo, t2)
     t1new += -2 * lib.einsum("lcki,lc,ka->ia", eris_ovoo, t1, t1)
     t1new += lib.einsum("kcli,lc,ka->ia", eris_ovoo, t1, t1)
+    intermediates["t1_new"] = t1new.copy()
+    return intermediates
 
-    # T2 equation
-    tmp2 = lib.einsum("kibc,ka->abic", eris.oovv, -t1)
-    tmp2 += np.asarray(eris_ovvv).conj().transpose(1, 3, 0, 2)
-    tmp = lib.einsum("abic,jc->ijab", tmp2, t1)
-    t2new = tmp + tmp.transpose(1, 0, 3, 2)
-    tmp2 = lib.einsum("kcai,jc->akij", eris.ovvo, t1)
-    tmp2 += eris_ovoo.transpose(1, 3, 0, 2).conj()
-    tmp = lib.einsum("akij,kb->ijab", tmp2, t1)
-    t2new -= tmp + tmp.transpose(1, 0, 3, 2)
-    t2new += np.asarray(eris.ovov).conj().transpose(0, 2, 1, 3)
-    if cc.cc2:
-        Woooo2 = np.asarray(eris.oooo).transpose(0, 2, 1, 3).copy()
-        Woooo2 += lib.einsum("lcki,jc->klij", eris_ovoo, t1)
-        Woooo2 += lib.einsum("kclj,ic->klij", eris_ovoo, t1)
-        Woooo2 += lib.einsum("kcld,ic,jd->klij", eris.ovov, t1, t1)
-        t2new += lib.einsum("klij,ka,lb->ijab", Woooo2, t1, t1)
-        Wvvvv = lib.einsum("kcbd,ka->abcd", eris_ovvv, -t1)
-        Wvvvv = Wvvvv + Wvvvv.transpose(1, 0, 3, 2)
-        Wvvvv += np.asarray(eris.vvvv).transpose(0, 2, 1, 3)
-        t2new += lib.einsum("abcd,ic,jd->ijab", Wvvvv, t1, t1)
-        Lvv2 = fvv - np.einsum("kc,ka->ac", fov, t1)
-        Lvv2 -= np.diag(np.diag(fvv))
-        tmp = lib.einsum("ac,ijcb->ijab", Lvv2, t2)
-        t2new += tmp + tmp.transpose(1, 0, 3, 2)
-        Loo2 = foo + np.einsum("kc,ic->ki", fov, t1)
-        Loo2 -= np.diag(np.diag(foo))
-        tmp = lib.einsum("ki,kjab->ijab", Loo2, t2)
-        t2new -= tmp + tmp.transpose(1, 0, 3, 2)
-    else:
-        Loo = imd.Loo(t1, t2, eris)
-        Lvv = imd.Lvv(t1, t2, eris)
-        Loo[np.diag_indices(nocc)] -= mo_e_o
-        Lvv[np.diag_indices(nvir)] -= mo_e_v
+    # # T2 equation
+    # tmp2 = lib.einsum("kibc,ka->abic", eris.oovv, -t1)
+    # tmp2 += np.asarray(eris_ovvv).conj().transpose(1, 3, 0, 2)
+    # tmp = lib.einsum("abic,jc->ijab", tmp2, t1)
+    # t2new = tmp + tmp.transpose(1, 0, 3, 2)
+    # tmp2 = lib.einsum("kcai,jc->akij", eris.ovvo, t1)
+    # tmp2 += eris_ovoo.transpose(1, 3, 0, 2).conj()
+    # tmp = lib.einsum("akij,kb->ijab", tmp2, t1)
+    # t2new -= tmp + tmp.transpose(1, 0, 3, 2)
+    # t2new += np.asarray(eris.ovov).conj().transpose(0, 2, 1, 3)
+    # if cc.cc2:
+    #     Woooo2 = np.asarray(eris.oooo).transpose(0, 2, 1, 3).copy()
+    #     Woooo2 += lib.einsum("lcki,jc->klij", eris_ovoo, t1)
+    #     Woooo2 += lib.einsum("kclj,ic->klij", eris_ovoo, t1)
+    #     Woooo2 += lib.einsum("kcld,ic,jd->klij", eris.ovov, t1, t1)
+    #     t2new += lib.einsum("klij,ka,lb->ijab", Woooo2, t1, t1)
+    #     Wvvvv = lib.einsum("kcbd,ka->abcd", eris_ovvv, -t1)
+    #     Wvvvv = Wvvvv + Wvvvv.transpose(1, 0, 3, 2)
+    #     Wvvvv += np.asarray(eris.vvvv).transpose(0, 2, 1, 3)
+    #     t2new += lib.einsum("abcd,ic,jd->ijab", Wvvvv, t1, t1)
+    #     Lvv2 = fvv - np.einsum("kc,ka->ac", fov, t1)
+    #     Lvv2 -= np.diag(np.diag(fvv))
+    #     tmp = lib.einsum("ac,ijcb->ijab", Lvv2, t2)
+    #     t2new += tmp + tmp.transpose(1, 0, 3, 2)
+    #     Loo2 = foo + np.einsum("kc,ic->ki", fov, t1)
+    #     Loo2 -= np.diag(np.diag(foo))
+    #     tmp = lib.einsum("ki,kjab->ijab", Loo2, t2)
+    #     t2new -= tmp + tmp.transpose(1, 0, 3, 2)
+    # else:
+    #     Loo = imd.Loo(t1, t2, eris)
+    #     Lvv = imd.Lvv(t1, t2, eris)
+    #     Loo[np.diag_indices(nocc)] -= mo_e_o
+    #     Lvv[np.diag_indices(nvir)] -= mo_e_v
 
-        Woooo = imd.cc_Woooo(t1, t2, eris)
-        Wvoov = imd.cc_Wvoov(t1, t2, eris)
-        Wvovo = imd.cc_Wvovo(t1, t2, eris)
-        Wvvvv = imd.cc_Wvvvv(t1, t2, eris)
+    #     Woooo = imd.cc_Woooo(t1, t2, eris)
+    #     Wvoov = imd.cc_Wvoov(t1, t2, eris)
+    #     Wvovo = imd.cc_Wvovo(t1, t2, eris)
+    #     Wvvvv = imd.cc_Wvvvv(t1, t2, eris)
 
-        tau = t2 + np.einsum("ia,jb->ijab", t1, t1)
-        t2new += lib.einsum("klij,klab->ijab", Woooo, tau)
-        t2new += lib.einsum("abcd,ijcd->ijab", Wvvvv, tau)
-        tmp = lib.einsum("ac,ijcb->ijab", Lvv, t2)
-        t2new += tmp + tmp.transpose(1, 0, 3, 2)
-        tmp = lib.einsum("ki,kjab->ijab", Loo, t2)
-        t2new -= tmp + tmp.transpose(1, 0, 3, 2)
-        tmp = 2 * lib.einsum("akic,kjcb->ijab", Wvoov, t2)
-        tmp -= lib.einsum("akci,kjcb->ijab", Wvovo, t2)
-        t2new += tmp + tmp.transpose(1, 0, 3, 2)
-        tmp = lib.einsum("akic,kjbc->ijab", Wvoov, t2)
-        t2new -= tmp + tmp.transpose(1, 0, 3, 2)
-        tmp = lib.einsum("bkci,kjac->ijab", Wvovo, t2)
-        t2new -= tmp + tmp.transpose(1, 0, 3, 2)
+    #     tau = t2 + np.einsum("ia,jb->ijab", t1, t1)
+    #     t2new += lib.einsum("klij,klab->ijab", Woooo, tau)
+    #     t2new += lib.einsum("abcd,ijcd->ijab", Wvvvv, tau)
+    #     tmp = lib.einsum("ac,ijcb->ijab", Lvv, t2)
+    #     t2new += tmp + tmp.transpose(1, 0, 3, 2)
+    #     tmp = lib.einsum("ki,kjab->ijab", Loo, t2)
+    #     t2new -= tmp + tmp.transpose(1, 0, 3, 2)
+    #     tmp = 2 * lib.einsum("akic,kjcb->ijab", Wvoov, t2)
+    #     tmp -= lib.einsum("akci,kjcb->ijab", Wvovo, t2)
+    #     t2new += tmp + tmp.transpose(1, 0, 3, 2)
+    #     tmp = lib.einsum("akic,kjbc->ijab", Wvoov, t2)
+    #     t2new -= tmp + tmp.transpose(1, 0, 3, 2)
+    #     tmp = lib.einsum("bkci,kjac->ijab", Wvovo, t2)
+    #     t2new -= tmp + tmp.transpose(1, 0, 3, 2)
 
-    eia = mo_e_o[:, None] - mo_e_v
-    eijab = lib.direct_sum("ia,jb->ijab", eia, eia)
-    t1new /= eia
-    t2new /= eijab
+    # eia = mo_e_o[:, None] - mo_e_v
+    # eijab = lib.direct_sum("ia,jb->ijab", eia, eia)
+    # t1new /= eia
+    # t2new /= eijab
 
-    return t1new, t2new
+    # return t1new, t2new
 
 
 npt = np.testing
@@ -114,7 +124,7 @@ ERROR_TOL = 1e-14
 
 
 # Generate CC quantities
-mol = gto.M(atom="H 0 -1 -1; O 0 0 0; H 0 1.2 -1;", basis="ccpvdz")
+mol = gto.M(atom="H 0 -1 -1; O 0 0 0; H 0 1.2 -1;", basis="3-21g")
 mf = scf.RHF(mol).run()
 mycc = cc.CCSD(mf)
 mycc.kernel()
@@ -126,9 +136,91 @@ eris = mycc.ao2mo()
 nocc, nvirt = t1.shape
 
 
+def update_amps_wrapper(t1, t2, eris):
+    """
+    void update_amps(
+    Eigen::Ref<RowMatrixXd> t1_mat,
+    Eigen::Ref<Eigen::VectorXd> t2_vec,
+    Eigen::Ref<RowMatrixXd> t1_mat_new,
+    Eigen::Ref<Eigen::VectorXd> t2_vec_new,
+    Eigen::Ref<RowMatrixXd> fock_oo_mat,
+    Eigen::Ref<RowMatrixXd> fock_ov_mat,
+    Eigen::Ref<RowMatrixXd> fock_vv_mat,
+    Eigen::Ref<Eigen::VectorXd> oooo_vec,
+    Eigen::Ref<Eigen::VectorXd> ovoo_vec,
+    Eigen::Ref<Eigen::VectorXd> oovv_vec,
+    Eigen::Ref<Eigen::VectorXd> ovvo_vec,
+    Eigen::Ref<Eigen::VectorXd> ovov_vec,
+    Eigen::Ref<Eigen::VectorXd> ovvv_vec,
+    Eigen::Ref<Eigen::VectorXd> vvvv_vec,
+    Eigen::Ref<Eigen::VectorXd> mo_energies)
+    """
+
+    t1_new = np.zeros_like(t1, order="C")
+    t2_new = np.zeros_like(t2, order="C")
+
+    fock = eris.fock
+    fov = np.ascontiguousarray(fock[:nocc, nocc:])
+    foo = np.ascontiguousarray(fock[:nocc, :nocc])
+    fvv = np.ascontiguousarray(fock[nocc:, nocc:])
+
+    args = [
+        t1,
+        t2.ravel(),
+        t1_new,
+        t2_new.ravel(),
+        foo.copy(),
+        fov.copy(),
+        fvv.copy(),
+        np.ascontiguousarray(eris.oooo).ravel(),
+        np.ascontiguousarray(eris.ovoo).ravel(),
+        np.ascontiguousarray(eris.oovv).ravel(),
+        np.ascontiguousarray(eris.ovvo).ravel(),
+        np.ascontiguousarray(eris.ovov).ravel(),
+        np.ascontiguousarray(eris.get_ovvv()).ravel(),
+        imd._get_vvvv(eris).ravel(),
+        np.ascontiguousarray(eris.mo_energy),
+    ]
+    for a in args:
+        print(a.data.contiguous, a.data.c_contiguous)
+    # exit(0)
+    my_update_amps(
+        t1,
+        t2.ravel(),
+        t1_new,
+        t2_new.ravel(),
+        foo.copy(),
+        fov.copy(),
+        fvv.copy(),
+        np.ascontiguousarray(eris.oooo).ravel(),
+        np.ascontiguousarray(eris.ovoo).ravel(),
+        np.ascontiguousarray(eris.oovv).ravel(),
+        np.ascontiguousarray(eris.ovvo).ravel(),
+        np.ascontiguousarray(eris.ovov).ravel(),
+        np.ascontiguousarray(eris.get_ovvv()).ravel(),
+        imd._get_vvvv(eris).ravel(),
+        np.ascontiguousarray(eris.mo_energy),
+    )
+
+    return t1_new, t2_new
+
+
 def test_update_amps():
     print()
     print("#" * 80)
     print("Testing `update_amps()`")
 
-    
+    intermediates = update_amps(mycc, t1, t2, eris)
+    pyscf_t1_new = intermediates["t1_new"]
+
+    my_t1_new, my_t2_new = update_amps_wrapper(t1, t2, eris)
+
+    print("Does pyscf t1 have inf?", np.isinf(pyscf_t1_new).any())
+    print("Does pyscf t1 have nan?", np.isnan(pyscf_t1_new).any())
+
+    print("Does my t1 have inf?", np.isinf(my_t1_new).any())
+    print("Does my t1 have nan?", np.isnan(my_t1_new).any())
+
+    error = np.linalg.norm(pyscf_t1_new - my_t1_new)
+    print(f"Error in t1_new {error:.2e}")
+    assert error < ERROR_TOL
