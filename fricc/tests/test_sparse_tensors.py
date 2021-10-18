8d4618bd04882854232081778903609bd9131d36
James E T Smith <james.smith9113@gmail.com>
2021-10-18 11:45:43 -0400

    Combining the fixes from the past two commits and fixing a problem with the tests that came up after refactoring

--- fricc/tests/test_sparse_tensors.py
+++ fricc/tests/test_sparse_tensors.py
@@ -58,7 +58,7 @@ def test_0101_contraction(no, nv, frac):
 
     t2_new_np = np.einsum("klij,klab->ijab", w, t2, order="C")
     t2_new_fri = np.zeros(t2.shape, order="C")
-    contract_DTSpT(w.ravel(), t2_compressed, t2_new_fri.ravel(), "0101")
+    contract_DTSpT(w, t2_compressed, t2_new_fri, "0101")
 
     npt.assert_almost_equal(t2_new_fri, t2_new_np)
 
@@ -93,7 +93,7 @@ def test_2323_contraction(no, nv, frac):
     # t2new += lib.einsum('abcd,ijcd->ijab', Wvvvv, tau)
     t2_new_np = np.einsum("abcd,ijcd->ijab", w, t2, order="C")
     t2_new_fri = np.zeros(t2.shape, order="C")
-    contract_DTSpT(w.ravel(), t2_compressed, t2_new_fri.ravel(), "2323")
+    contract_DTSpT(w, t2_compressed, t2_new_fri, "2323")
     npt.assert_almost_equal(t2_new_fri, t2_new_np)
 
 
@@ -119,7 +119,7 @@ def test_1302_contraction(no, nv, frac):
 
     t2_new_np = 2 * np.einsum("akic,kjcb->ijab", w, t2, order="C")
     t2_new_fri = np.zeros(t2.shape, order="C")
-    contract_DTSpT(w.ravel(), t2_compressed, t2_new_fri.ravel(), "1302")
+    contract_DTSpT(w, t2_compressed, t2_new_fri, "1302")
 
     npt.assert_almost_equal(t2_new_fri, t2_new_np)
 
@@ -146,7 +146,7 @@ def test_1202_contraction(no, nv, frac):
 
     t2_new_np = -1 * np.einsum("akci,kjcb->ijab", w, t2, order="C")
     t2_new_fri = np.zeros(t2.shape, order="C")
-    contract_DTSpT(w.ravel(), t2_compressed, t2_new_fri.ravel(), "1202")
+    contract_DTSpT(w, t2_compressed, t2_new_fri, "1202")
 
     npt.assert_almost_equal(t2_new_fri, t2_new_np)
 
@@ -173,7 +173,7 @@ def test_1303_contraction(no, nv, frac):
 
     t2_new_np = np.einsum("akic,kjbc->ijab", w, t2, order="C")
     t2_new_fri = np.zeros(t2.shape, order="C")
-    contract_DTSpT(w.ravel(), t2_compressed, t2_new_fri.ravel(), "1303")
+    contract_DTSpT(w, t2_compressed, t2_new_fri, "1303")
 
     npt.assert_almost_equal(t2_new_fri, t2_new_np)
 
@@ -200,7 +200,7 @@ def test_1203_contraction(no, nv, frac):
 
     t2_new_np = np.einsum("bkci,kjac->ijab", w, t2, order="C")
     t2_new_fri = np.zeros(t2.shape, order="C")
-    contract_DTSpT(w.ravel(), t2_compressed, t2_new_fri.ravel(), "1203")
+    contract_DTSpT(w, t2_compressed, t2_new_fri, "1203")
 
     npt.assert_almost_equal(t2_new_fri, t2_new_np)
 
@@ -227,7 +227,7 @@ def test_1323_contraction(no, nv, frac):
 
     Wklij_np = np.einsum("kcld,ijcd->klij", ovov, t2, order="C")
     Wklij = np.zeros(Wklij_np.shape, order="C")
-    contract_DTSpT(ovov.ravel(), t2_compressed, Wklij.ravel(), "1323")
+    contract_DTSpT(ovov, t2_compressed, Wklij, "1323")
 
     npt.assert_almost_equal(Wklij, Wklij_np)
 
@@ -254,7 +254,7 @@ def test_0112_contraction(no, nv, frac):
 
     Wakic_np = -0.5 * np.einsum("ldkc,ilda->akic", ovov, t2, order="C")
     Wakic = np.zeros(Wakic_np.shape, order="C")
-    contract_DTSpT(ovov.ravel(), t2_compressed, Wakic.ravel(), "0112")
+    contract_DTSpT(ovov, t2_compressed, Wakic, "0112")
 
     npt.assert_almost_equal(Wakic, Wakic_np)
 
@@ -281,7 +281,7 @@ def test_0113_contraction(no, nv, frac):
 
     Wakic_np = np.einsum("ldkc,ilad->akic", ovov, t2, order="C")
     Wakic = np.zeros(Wakic_np.shape, order="C")
-    contract_DTSpT(ovov.ravel(), t2_compressed, Wakic.ravel(), "0113")
+    contract_DTSpT(ovov, t2_compressed, Wakic, "0113")
 
     npt.assert_almost_equal(Wakic, Wakic_np)
 
@@ -308,7 +308,7 @@ def test_0313_contraction(no, nv, frac):
 
     Wakic_np = -0.5 * np.einsum("lckd,ilad->akic", ovov, t2, order="C")
     Wakic = np.zeros(Wakic_np.shape, order="C")
-    contract_DTSpT(ovov.ravel(), t2_compressed, Wakic.ravel(), "0313")
+    contract_DTSpT(ovov, t2_compressed, Wakic, "0313")
 
     npt.assert_almost_equal(Wakic, Wakic_np)
 
@@ -335,6 +335,6 @@ def test_0312_contraction(no, nv, frac):
 
     Wakci_np = -0.5 * np.einsum("lckd,ilda->akci", ovov, t2, order="C")
     Wakci = np.zeros(Wakci_np.shape, order="C")
-    contract_DTSpT(ovov.ravel(), t2_compressed, Wakci.ravel(), "0312")
+    contract_DTSpT(ovov, t2_compressed, Wakci, "0312")
 
     npt.assert_almost_equal(Wakci, Wakci_np)
